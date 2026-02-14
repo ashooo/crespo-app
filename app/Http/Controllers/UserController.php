a46ffe9bde9020033076ef09d3b89e87cd813438
crespo <crespo_karljohn@plpasig.edu.ph>
2026-02-14 23:58:40 +0800

    exercise #2 - routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\UserService;
+
+class UserController extends Controller
+{
+    public function show() {
+        $users = [
+            [
+                "name" => "John Doe",
+                "gender" => "Male"
+            ],
+            [
+                "name" => "Jane Doe",
+                "gender" => "Female"
+            ]
+        ];
+
+        return response()->json($users); 
+    }
+
+    public function index(UserService $userService) {
+        return response()->json($userService->listUsers());
+    }
+}
